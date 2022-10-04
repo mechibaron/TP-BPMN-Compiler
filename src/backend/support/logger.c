466dfa9f6a437103c1c8b54225615229a3f04522
Agustina Ortu <aortu@itba.edu.ar>
2022-10-04 19:46:43 -0300

    logger

--- src/backend/support/logger.c
+++ src/backend/support/logger.c
@@ -4,42 +4,38 @@
  #include <string.h>
 
 
-/**
- * Implementación de "logger.h".
- /
-
-void Log(FILE const stream, const char * prefix, const char * const format, const char * suffix, va_list arguments) {
-    fprintf(stream, "%s", prefix);
-    vfprintf(stream, format, arguments);
-    fprintf(stream, "%s", suffix);
+void Log(FILE * const stream, const char * prefix, const char * const format, const char * suffix, va_list arguments) {
+	fprintf(stream, "%s", prefix);
+	vfprintf(stream, format, arguments);
+	fprintf(stream, "%s", suffix);
 }
 
 void LogDebug(const char * const format, ...) {
-    #ifdef DEBUG
-    va_list arguments;
-    va_start(arguments, format);
-    Log(stdout, "[DEBUG] ", format, "\n", arguments);
-    va_end(arguments);
-    #endif
+	#ifdef DEBUG
+	va_list arguments;
+	va_start(arguments, format);
+	Log(stdout, "[DEBUG] ", format, "\n", arguments);
+	va_end(arguments);
+	#endif
 }
 
 void LogError(const char * const format, ...) {
-    va_list arguments;
-    va_start(arguments, format);
-    Log(stderr, "[ERROR] ", format, "\n", arguments);
-    va_end(arguments);
+	va_list arguments;
+	va_start(arguments, format);
+	Log(stderr, "[ERROR] ", format, "\n", arguments);
+	va_end(arguments);
 }
 
 void LogErrorRaw(const char * const format, ...) {
-    va_list arguments;
-    va_start(arguments, format);
-    Log(stderr, "", format, "", arguments);
-    va_end(arguments);
+	va_list arguments;
+	va_start(arguments, format);
+	Log(stderr, "", format, "", arguments);
+	va_end(arguments);
 }
 
 void LogInfo(const char * const format, ...) {
-    va_list arguments;
-    va_start(arguments, format);
-    Log(stdout, "[INFO ] ", format, "\n", arguments);
-    va_end(arguments);
+	va_list arguments;
+	va_start(arguments, format);
+	Log(stdout, "[INFO ] ", format, "\n", arguments);
+	va_end(arguments);
 }
